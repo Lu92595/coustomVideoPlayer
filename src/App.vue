bb9f13bfc91f681e5affa7329c0a772ccee8bdfd
zach.zqy <bianzhixiaoyuan@163.com>
2023-11-17 09:38:12 +0800

    333

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import Display from "./components/Display.vue";
+import { reactive } from "vue";
+
+const globalData = reactive({ value: "love & peace" });
+const setGlobalData = (obj: any) => {
+  globalData.value = obj.value;
+}
 </script>
 
 <template>
-  <Display value="Hello Zion" :global-data="{name:'444'}" :set-global-data="() => {}" />
+  <Display :value="globalData?.value" :global-data="globalData" :set-global-data="setGlobalData" />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,44 +1,68 @@
 <template>
   <div>
-    <video ref="videoPlayer" controls @loadedmetadata="loadedmetadata" @timeupdate="updateProgress">
+    <div class="container">
+      <h1>this is a Vue3 Component</h1>
+      <div>value from outer globalData.value: {{ value || "no input" }}</div>
+      <input @input="onInput" />
+    </div>
+
+    <video ref="videoPlayer" width="600" controls @timeupdate="updateProgress" @loadedmetadata="loadedmetadata">
       <source :src="videoSource" type="video/mp4">
       Your browser does not support the video tag.
     </video>
+
     <div>
       <p>当前播放时间: {{ currentTime }} 秒</p>
       <input type="range" v-model="currentTime" :max="duration" @input="seekTo">
     </div>
+
   </div>
 </template>
 
-<script>
-export default {
-  props: ['globalData'],
-  data() {
-    return {
-      videoSource: '/video.mp4',
-      currentTime: 30, // 设置默认进度为30秒
-      duration: 0,
-    };
-  },
-  methods: {
-    updateProgress() {
-      this.currentTime = this.$refs.videoPlayer.currentTime;
-    },
-    seekTo() {
-      this.$refs.videoPlayer.currentTime = this.currentTime;
-    },
-    // 监听视频加载完成事件
-    loadedmetadata() {
-      console.log("globalData:", this.globalData);
-      this.$refs.videoPlayer.currentTime = 12;
-    }
-  },
-  mounted() {
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+const props = defineProps<{
+  globalData: Record<string, any>;
+  setGlobalData: (newValue: any) => void;
+  value?: string;
+}>();
+const videoPlayer = ref<HTMLVideoElement | null>(null);
+const videoSource = '/video.mp4';
+const currentTime = ref<number>(0);
+const duration = ref<number>(0);
 
-  },
+const onInput = (e: Event) => {
+  props.setGlobalData({
+    ...props.globalData,
+    value: (e.target as HTMLInputElement)?.value,
+  });
+}
+const updateProgress = () => {
+  if (videoPlayer.value) {
+    currentTime.value = videoPlayer.value.currentTime;
+  }
 };
+
+const seekTo = () => {
+  if (videoPlayer.value) {
+    videoPlayer.value.currentTime = currentTime.value;
+
+  }
+};
+
+const loadedmetadata = () => {
+  if (videoPlayer.value) {
+    videoPlayer.value.currentTime = 20;
+  }
+}
+
+onMounted(() => {
+
+});
+
+onUnmounted(() => {
+  if (videoPlayer.value) {
+    videoPlayer.value.removeEventListener('loadedmetadata', () => { });
+  }
+});
 </script>
-
-
-
