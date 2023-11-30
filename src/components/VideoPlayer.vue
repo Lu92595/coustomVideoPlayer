1c3baadda929b8f3826052b05820ef3fb25118cb
zach.zqy <bianzhixiaoyuan@163.com>
2023-11-30 12:52:09 +0800

    1.0.2

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -59,13 +59,32 @@ export default {
     },
     // 视频进度更新
     async postMaxCurrentTime() {
-      this.currentTime = this.player.getCurrentTime();
+      const now_time = new Date().getTime();
+      const playerStatus = this.player.getStatus()
+      const currentTime = this.player.getCurrentTime();
 
-      // 播放进度超过最大进度时
-      if (this.currentTime > this.maxCurrentTime && this.player.getStatus() == "playing") {
-        const now_time = new Date().getTime();
+      // 刻度超过5s说明是拖动了进度条
+      if (currentTime - this.currentTime > 5) {
+        if (currentTime > this.maxCurrentTime) {
+          console.log("seek-maxCurrentTime", this.maxCurrentTime);
+          this.player.seek(this.maxCurrentTime)
+          this.currentTime = this.maxCurrentTime;
+        } else {
+          console.log("seek-currentTime", currentTime);
+          this.player.seek(currentTime)
+          this.currentTime = currentTime;
+        }
+      }
+      else if (currentTime < this.maxCurrentTime) {
+        // 正常的进度播放还没到达最大进度
+        this.currentTime = currentTime;
+      }
+      else if (currentTime >= this.maxCurrentTime) {
+        // 播放到最大进度后正常播放
+        this.currentTime = currentTime;
+
         // 每秒钟更新下本地的最大进度
-        if (this.lastUpdateTime && now_time - this.lastWatchTime > 1000 * 1) {
+        if (now_time - this.lastWatchTime > 1000 * 1) {
           this.maxCurrentTime = this.currentTime;
           this.lastWatchTime = now_time;
         }
@@ -148,10 +167,10 @@ export default {
         //挂载事件
         this.onReady();
         this.onTimeupdate();
-        this.onStartSeek();
-        this.onCompleteSeek();
-        this.onPlay();
-        this.onPause();
+        // this.onStartSeek();
+        // this.onCompleteSeek();
+        // this.onPlay();
+        // this.onPause();
       }
 
     },
@@ -169,44 +188,27 @@ export default {
         await this.postMaxCurrentTime();
       });
     },
-    //开始播放时
-    onPlay() {
-      this.player.on('play', res => {
-        console.log("play", res);
-        const currentTime = this.player.getCurrentTime();
-        if (currentTime > this.maxCurrentTime) {
-          this.player.seek(this.maxCurrentTime)
-        }
-      });
-    },
-    // 播放到暂停时
-    onPause() {
-      this.player.on('pause', res => {
-        console.log("pause", res);
-        const currentTime = this.player.getCurrentTime();
-        if (currentTime > this.maxCurrentTime) {
-          this.player.seek(this.maxCurrentTime)
-        }
-      });
-    },
-    // 开始拖动进度条时
-    onStartSeek() {
-      this.player.on('startSeek', (res) => {
-        console.log("startSeek", res);
-        this.player.pause()
-      });
-    },
-    // 进度条拖动完成时
-    onCompleteSeek() {
-      this.player.on('completeSeek', res => {
-        console.log("completeSeek", res);
-        const timer = setTimeout(() => {
-          clearTimeout(timer)
-          this.player.play()
-        }, 300)
+    // //开始播放时
+    // onPlay() {
+    //   this.player.on('play', res => {
+    //   });
+    // },
+    // // 播放到暂停时
+    // onPause() {
+    //   this.player.on('pause', res => {
 
-      });
-    }
+    //   });
+    // },
+    // // 开始拖动进度条时
+    // onStartSeek() {
+    //   this.player.on('startSeek', (res) => {
+    //   });
+    // },
+    // // 进度条拖动完成时
+    // onCompleteSeek() {
+    //   this.player.on('completeSeek', res => {
+    //   });
+    // }
   }
 }
 </script>
